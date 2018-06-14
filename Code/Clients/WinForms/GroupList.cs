532583e59471ad1b53325fb2346cf9cfc2020f99
NhoxLeo <37131285+NhoxLeo@users.noreply.github.com>
2018-06-15 06:10:49 +0700

    + Update

--- Code/Clients/WinForms/GroupList.cs
+++ Code/Clients/WinForms/GroupList.cs
@@ -14,10 +14,11 @@ namespace Clients
     public partial class GroupList : Form
     {
         Client_module myClient;
+        List<string> clientList = new List<string>();
         public GroupList()
         {
             InitializeComponent();
-            InitializeButtons();
+            //InitializeButtons();
         }
         public void SetClientModule(Client_module _Module)
         {
@@ -31,18 +32,20 @@ namespace Clients
             foreach (string name in listClients)
             {
                 result = result + " " + name;
+                clientList.Add(name);
             }
             MessageBox.Show("Current clients " + result);
+
         }
 
-        void InitializeButtons()
+        void InitializeButtons(List<string> _clients)
         {
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < _clients.Capacity; i++)
             {
                 FlowLayoutPanel panel = new FlowLayoutPanel() { Height = buttonFlowLayoutPanel.ClientSize.Height - 280, Width = buttonFlowLayoutPanel.ClientSize.Width };
                 Label label = new Label() { Width = panel.ClientSize.Width - 200 , TextAlign = ContentAlignment.MiddleLeft };
                 Button button = new Button() { Height = label.ClientSize.Height };
-                label.Text = "User " + i;
+                label.Text = _clients[i];
                 button.Text = "Begin Messaging";
                 button.Click += new EventHandler(OpenChatbox);
                 panel.Controls.Add(label);
